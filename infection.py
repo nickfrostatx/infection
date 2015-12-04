0d103e7cea5747674fa5173f8b1e1ed6a0cc57e2
Nick Frost <nickfrostatx@gmail.com>
2015-12-04 12:28:46 -0800

    Add User model and total_infection

--- infection.py
+++ infection.py
@@ -2,8 +2,37 @@
 """The code."""
 
 
+class User(object):
+
+    """The user model."""
+
+    def __init__(self, id):
+        """Create a new user with the given id."""
+        self.id = id
+        self.adjacent = set()
+        self.new_version = False
+
+    def connect(self, other):
+        """Add a new relationship."""
+        self.adjacent.add(other)
+        other.adjacent.add(self)
+
+
+def total_infection(user):
+    """Infect all the users connected to user.
+
+    This is implemented with a breadth-first search.
+    """
+    if user.new_version:
+        return
+    user.new_version = True
+    for adjacent in user.adjacent:
+        total_infection(adjacent)
+
+
 def main():
     """The main function."""
+
 
 if __name__ == '__main__':
     main()
--- test_infection.py
+++ test_infection.py
@@ -1,8 +1,33 @@
 # -*- coding: utf-8 -*-
 """Test cases for infection.py, run using pytest."""
 
-from infection import main
+from infection import User, total_infection
 
 
-def test_main():
-    main()
+def test_user_model():
+    """Test the user constructor."""
+    user1 = User(1)
+    assert user1.id == 1
+    assert not user1.new_version
+
+
+def test_user_connect():
+    """Test the graph connection behavior."""
+    user1 = User(1)
+    user2 = User(2)
+    user1.connect(user2)
+    assert user2 in user1.adjacent
+    assert user1 in user2.adjacent
+
+
+def test_total_infection():
+    """This will test with a circular graph (0->1->2->0...)."""
+    users = [User(i) for i in range(4)]
+    users[0].connect(users[1])
+    users[1].connect(users[2])
+    users[2].connect(users[0])
+    total_infection(users[0])
+    for i in range(3):
+        assert users[i].new_version
+    # User 3 should never be visited.
+    assert not users[3].new_version
