44a0c746df716a2fa92f2aca0fe10f6a21c5913d
Nick Frost <nickfrostatx@gmail.com>
2015-12-04 15:28:18 -0800

    Add Infection model

--- infection.py
+++ infection.py
@@ -6,33 +6,68 @@ class User(object):
 
     """The user model."""
 
-    def __init__(self, id):
+    def __init__(self, id, version=None):
         """Create a new user with the given id."""
         self.id = id
         self.adjacent = set()
-        self.new_version = False
+        self._version = version
+        self.infection = None
 
     def connect(self, other):
         """Add a new relationship."""
         self.adjacent.add(other)
         other.adjacent.add(self)
 
+    @property
+    def version(self):
+        """Try to get the version from the infection.
 
-def total_infection(user):
+        If we aren't part of an infection, just return _version.
+        """
+        if self.infection is not None and self.infection.version is not None:
+                return self.infection.version
+        return self._version
+
+
+class Infection(set):
+
+    """A group of connected users, all running the same version."""
+
+    def __init__(self, version=None):
+        """If specified, version will be used by all contained users."""
+        set.__init__(self)
+        self.version = version
+
+
+def infect(user, infection=None):
+    """Add all users connected to user to a common infection group.
+
+    Return the infection.
+
+    This is just a DFS, should run in O(n) time.
+    """
+    if user.infection is not None:
+        return user.infection
+
+    # Default to a new infection
+    if infection is None:
+        infection = Infection()
+
+    infection.add(user)
+    user.infection = infection
+    for adjacent in user.adjacent:
+        infect(adjacent, infection)
+
+    return infection
+
+
+def total_infection(user, new_version):
     """Infect all the users connected to user.
 
-    This generator is implemented with a depth-first search. Each node
-    is yielded as it is infected.
+    This runs in O(n).
     """
-    if user.new_version:
-        return
-
-    user.new_version = True
-    yield user
-
-    for adjacent in user.adjacent:
-        for infected in total_infection(adjacent):
-            yield infected
+    infection = infect(user)
+    infection.version = new_version
 
 
 def main():
--- test_infection.py
+++ test_infection.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 """Test cases for infection.py, run using pytest."""
 
-from infection import User, total_infection
+from infection import User, Infection, infect, total_infection
+import pytest
+
+
+@pytest.fixture
+def circular_users():
+    num_users = 3
+    users = [User(i, 1) for i in range(num_users)]
+    for i in range(num_users):
+        users[i].connect(users[(i + 1) % num_users])
+    return users
 
 
 def test_user_model():
     """Test the user constructor."""
-    user1 = User(1)
+    user1 = User(1, 1)
     assert user1.id == 1
-    assert not user1.new_version
+    assert user1.version == 1
 
 
 def test_user_connect():
@@ -20,10 +30,26 @@ def test_user_connect():
     assert user1 in user2.adjacent
 
 
-def test_total_infection():
+def test_infection_model():
+    """Make sure users pull version from their infection."""
+    user = User(1, 1)
+    assert user.version == 1
+    infection = Infection(2)
+    user.infection = infection
+    infection.add(user)
+    assert user.version == 2
+
+
+def test_infect(circular_users):
+    """Check our infect model on a circular graph"""
+    infection = infect(circular_users[0])
+    for user in circular_users:
+        assert user in infection
+        assert user.infection == infection
+
+
+def test_total_infection(circular_users):
     """This will test with a circular graph (0->1->2->0...)."""
-    users = [User(i) for i in range(4)]
-    users[0].connect(users[1])
-    users[1].connect(users[2])
-    users[2].connect(users[0])
-    assert list(total_infection(users[0])) == users[:3]
+    total_infection(circular_users[0], 2)
+    for user in circular_users:
+        assert user.version == 2
